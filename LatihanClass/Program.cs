0b6adee03f65750c6435360b6fea36218279404e
faruqputraa <faruqputraa@students.amikom.ac.id>
2023-03-29 20:08:36 +0700

    first commit

---
+++ LatihanClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using LatihanClassTaxi;
+
+namespace LatihanClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pengambilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+                
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LatihanClass/Taxi.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassTaxi
+{
+    public class Taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverName: {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("OnDuty: Yes", OnDuty);
+            }
+            else
+            {
+                Console.WriteLine("OnDuty: No", OnDuty);
+            }
+            Console.WriteLine("NumPassenger: {0}\n", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);    
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine( "{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
+
+
